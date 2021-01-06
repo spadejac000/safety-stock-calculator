e0389ec4c4147dc09734a997accf13ed2bb5dcad
Jacob Spade <spade.jacob@Yahoo.com>
2021-01-05 18:39:47 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const result = () => {
+  let inputs = document.querySelectorAll('.input')
+  let mdu = document.getElementById('mdu').value;
+  let mlt = document.getElementById('mlt').value;
+  let adu = document.getElementById('adu').value;
+  let alt = document.getElementById('alt').value;
+  let output = (parseInt(mdu) * parseInt(mlt)) - (parseInt(adu) * parseInt(alt))
+  if(isNaN(output)) {
+    document.getElementById('answer').innerHTML = "Please provide valid inputs"
+  } else {
+    document.getElementById('answer').innerHTML = output;
+  }
+
+  const htmlValue = () => {
+    if(isNaN(output)) {
+      document.getElementById('answer').innerHTML = "Please provide valid inputs"
+    } else {
+      document.getElementById('answer').innerHTML = output;
+    }
+  }
+
+
+  inputs.forEach(input => {
+    input.addEventListener('change', htmlValue)
+  })
+}
